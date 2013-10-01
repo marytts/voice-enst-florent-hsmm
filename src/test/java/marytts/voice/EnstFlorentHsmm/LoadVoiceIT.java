2aaf489c194b31000dc88aba3862621008bf8c51
Ingmar Steiner <steiner@coli.uni-saarland.de>
2013-10-01 17:05:01 +0200

    convert voice to MaryTTS 5
    incantation:
    $ rm -f mary-florent_fr_hmm-4.3.0.zip
    $ zip -r mary-florent_fr_hmm-4.3.0.zip conf lib
    $ java -cp $MARYBASE/lib/marytts-builder-5.1-SNAPSHOT-jar-with-dependencies.jar marytts.tools.upgrade.Mary4To5VoiceConverter mary-florent_fr_hmm-4.3.0-component.xml mary-florent_fr_hmm-4.3.0.zip

---
+++ src/test/java/marytts/voice/EnstFlorentHsmm/LoadVoiceIT.java
@@ -0,0 +1,23 @@
+package marytts.voice.EnstFlorentHsmm;
+
+import static org.junit.Assert.assertNotNull;
+import marytts.modules.synthesis.Voice;
+import marytts.util.MaryRuntimeUtils;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoadVoiceIT {
+	
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		MaryRuntimeUtils.ensureMaryStarted();
+	}
+	
+    @Test
+    public void canLoadVoice() throws Exception {
+    	Config config = new Config();
+        Voice voice = new marytts.htsengine.HMMVoice(config.getName(), null);
+        assertNotNull(voice);
+    }
+}
